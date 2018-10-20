24136b0d883deebe3aecb98e707fa7dbd316bbd3
toddnguyen47 <todd.nguyen47@gmail.com>
2018-10-20 00:32:29 -0700

    Adding costs to unique units/techs

--- aoe2/index-aoe.js
+++ aoe2/index-aoe.js
@@ -67,3 +67,24 @@ function createLi(link, name, description) {
 		.appendTo(li);
 	return li;
 }
+
+// Reference
+// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	// Default scroll distance is 20
+	var scrollDistance = 200;  // 20 = default
+	if (document.body.scrollTop > scrollDistance || document.documentElement.scrollTop > scrollDistance) {
+		document.getElementById("toTopButton").style.display = "block";
+	} else {
+		document.getElementById("toTopButton").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
