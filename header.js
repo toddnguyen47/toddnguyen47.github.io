619df7b18493dd72fc2a6e2479f9734f2f008831
Todd Nguyen <todd.nguyen47@gmail.com>
2019-01-31 15:34:20 -0800

    Added navigation bar

--- header.js
+++ header.js
@@ -1,57 +1,52 @@
 $(document).ready(function() {
-	// Load the pseudo navigation bar,
-	// Then set the title once the load is compplete
-	$("#topMenu").load("/header.html", null, setTitleNavBar);
-	
-	
-	/**
-	 * Submenu code
-	 * Reference: https://stackoverflow.com/a/45755948
-	 */	
-	console.log("DELETE ME");
-	// When the dropdown toggle is clicked
-	$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-		console.log("WE IN EHRE");
-		// If the very next sibling element does not have the show class
-		if (!$(this).next().hasClass('show')) {
-			// Obtain the first parent that has the dropdown-menu class
-			// then find the descendants of that first parent and remove all
-			// show class
-			$(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
-		}
-	
-		// The submenu is the next dropdown menu, e.g. never the initial button
-		var $subMenu = $(this).next("dropdown-menu");
-		$subMenu.toggleClass('show');  // TOGGLE, not solely turning it on!
-	
-		// More stuff
-		console.log($subMenu.attr('class'));
+	$("#topMenu").load("/header.html", function(responseText, textStatus, jqXHR) {
+		setActivePage();
+		addSubMenuFunctionality();
 	});
-	console.log("DELETE ME 2");
+});
+
+/**
+ * Set the current active page on the navigation bar
+ */
+function setActivePage() {
+	var dictPages = {
+		"": "#navbarHome",
+		"ff6": "#navbarGamesDropdownMenuLink",
+		"aoe2": "#navbarGamesDropdownMenuLink",
+		"dnd": "#navbarDnD"
+	};
+
+	// Current page that we are on
+	var pgUrl = window.location.pathname.substr(1, window.location.pathname.lastIndexOf("/") - 1).trim();
+	var activePage = dictPages[pgUrl];
+
+	$(activePage).addClass("active");
+	// TODO: Do we have to remove any current active classes?
+}
 
 
-	/**
-	 * Set the title of the "nav bar" according to what page it is currently on.
-	 */
-	function setTitleNavBar() {
-		var titleDict = {
-			"": "Home",
-			"aoe2": "Age of Empires 2",
-			"dnd" : "Dungeons and Dragons Spells",
-			"ff6" : "Final Fantasy VI Advance Utilities",
-			"replaceSlashes": "Replace Slashes"
-		};
-
-		// Current page that we are on
-		var pgUrl = window.location.pathname.substr(1, window.location.pathname.lastIndexOf("/") - 1).trim();
-		var properTitle = titleDict[pgUrl];
-
-		var maxLength = 15;
-		if (properTitle.length > maxLength) {
-			// We want to subtract 3 to accommodate for the ellipses
-			properTitle = properTitle.substr(0, maxLength - 3) + "...";
+/**
+ * Add submenu functionality
+ * Reference: https://stackoverflow.com/q/44467377
+ */
+function addSubMenuFunctionality() {
+	// Any dropdown toggle after dropdown-menu, i.e. a submenu!
+	$(".dropdown-menu a.dropdown-toggle").on("click", function(e) {
+		// Right now, we are at the anchor link
+		// Close ALL other submenus
+		if (!$(this).next().hasClass('show')) {
+			$(this).parents(".dropdown-menu").first().find(".show").removeClass("show");
 		}
 
-		$("#menuBtn span.menuBtnText").text(properTitle);
-	}
-});
+		// Show/hide the current interested submenu
+		var submenu = $(this).next(".dropdown-menu");
+		submenu.toggleClass("show");
+
+		// If parent closes, close the submenus
+		$(this).parents("li.nav-item.dropdown.show").on("hidden.bs.dropdown", function(e) {
+			$(".dropdown-submenu .show").removeClass("show");
+		});
+		
+		e.stopPropagation();
+	});
+}
