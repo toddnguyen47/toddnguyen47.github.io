026842dcc2eaee519883f1fde2b74484341a90c7
toddnguyen47 <todd.nguyen47@gmail.com>
2019-02-06 03:05:59 -0800

    Implemented pseudo-JS objects oriented

--- aoe2/Beginner_Guide/beginnerGuide.js
+++ aoe2/Beginner_Guide/beginnerGuide.js
@@ -1,31 +1,18 @@
-var maxMobileWindowWidth = 575;
-
-// Remember which military civilization was shown last
-var lastShownCivCardId1 = "#aztecsContent";
-
-// The alphabetically sorted list of civs
-// Used for loadCivTabs() and loadCivMilitaryComposition()
-var civList = ["Aztecs","Berbers","Britons","Burmese","Byzantines","Celts","Chinese",
-	"Ethiopians","Franks","Goths","Huns","Incas","Indians","Italians","Japanese","Khmer",
-	"Koreans","Magyars","Malay","Malians","Mayans","Mongols","Persians","Portuguese",
-	"Saracens","Slavs","Spanish","Teutons","Turks","Vietnamese","Vikings"];
-var activeCiv = "Aztecs";
-
-$(document).ready(function(){
+$(document).ready(function() {
 	addSidebarActive();
 
-	// Check for initial window width
-	addDynamicResizing();
+	// // Check for initial window width
+	var dynamicResizeObj = addDynamicResizing();
 	// Then when window changes size, check dynamically
-	$(window).resize(addDynamicResizing);
+	$(window).resize(dynamicResizeObj.windowWidthFunc);
 
+	dynamicResizeObj.addSidebarLinkClickHandler();
 	addSidebarButtonToggleFunctionality();
-	addSidebarLinkClickHandler();
 
-	addMilitaryCardUnitFunc();
-
-	loadCivTabs();
-	loadCivMilitaryComposition();
+	militaryCivObject = getMilitaryCivObject();
+	militaryCivObject.loadCivTabs();
+	militaryCivObject.loadCivMilitaryComposition();
+	militaryCivObject.addMilitaryCardUnitFunc();
 });
 
 
@@ -33,7 +20,7 @@ $(document).ready(function(){
  * Set the sidebar to active whenever it is clicked
  */
 function addSidebarActive() {
-	$(".nav-item a.nav-link").on("click", function(e) {
+	$(".sidebarNav .nav-item a.nav-link").on("click", function(e) {
 		// Remove all active classes
 		$(this).parents(".nav").first().find("a.nav-link").removeClass("active");
 		// Add the current class to be the only active class
@@ -43,19 +30,43 @@ function addSidebarActive() {
 
 
 /**
- * When you resize, hide certain elements
+ * Add dynamic resizing options for the page.
  */
 function addDynamicResizing() {
-	var $windowWidth = $(window).width();
-	// If mobile
-	if ($windowWidth < maxMobileWindowWidth) {
-		$("div#sidebarNavContent").addClass("customHidden");
+	var maxMobileWindowWidth = 575;
+
+	var windowWidthFunc = function() {
+		var $windowWidth = $(window).width();
+		// If mobile
+		if ($windowWidth < maxMobileWindowWidth) {
+			$("div#sidebarNavContent").addClass("customHidden");
+		}
+
+		// If not mobile
+		else {
+			$("div#sidebarNavContent").removeClass("customHidden");
+		}
 	}
 
-	// If not mobile
-	else {
-		$("div#sidebarNavContent").removeClass("customHidden");
+
+	/**
+	 * MOBILE ONLY
+	 * When you click on a sidebar link, hide the sidebar
+	 */
+	var addSidebarLinkClickHandler = function() {
+		$(".nav-item a.nav-link").on("click", function(e) {
+			var $windowWidth = $(window).width();
+			// If mobile, hide the sidebar
+			if ($windowWidth < maxMobileWindowWidth) {
+				$(this).parents(".sidebarNav").addClass("customHidden");
+			}
+		});
 	}
+
+	return {
+		addSidebarLinkClickHandler: addSidebarLinkClickHandler,
+		windowWidthFunc: windowWidthFunc
+	};
 }
 
 
@@ -71,108 +82,134 @@ function addSidebarButtonToggleFunctionality() {
 
 
 /**
- * MOBILE ONLY
- * When you click on a sidebar link, hide the sidebar
+ * Pseudo-object that deals with civilizations
  */
-function addSidebarLinkClickHandler() {
-	$(".nav-item a.nav-link").on("click", function(e) {
-		var $windowWidth = $(window).width();
-		// If mobile, hide the sidebar
-		if ($windowWidth < maxMobileWindowWidth) {
-			$(this).parents(".sidebarNav").addClass("customHidden");
-		}
-	});
-}
+function getMilitaryCivObject() {
+	// The alphabetically sorted list of civs
+	// Used for loadCivTabs() and loadCivMilitaryComposition()
+	var m_civList = ["Aztecs","Berbers","Britons","Burmese","Byzantines","Celts","Chinese",
+		"Ethiopians","Franks","Goths","Huns","Incas","Indians","Italians","Japanese","Khmer",
+		"Koreans","Magyars","Malay","Malians","Mayans","Mongols","Persians","Portuguese",
+		"Saracens","Slavs","Spanish","Teutons","Turks","Vietnamese","Vikings"];
+	// Randomly choose a last shown civ
+	var randomIndex = Math.floor(Math.random() * m_civList.length);
+	var m_activeCiv = m_civList[randomIndex];
 
-
-/**
- * Add functionality to the military cards
- */
-function addMilitaryCardUnitFunc() {
-	$("#aztecsBerbersTab a").on("click", function(e) {
-		e.preventDefault();
-		// Obtain the ID associated with the tab
-		var tabId = $(this)[0]["hash"];
-
-		// Turn off previously active card
-		$(lastShownCivCardId1).removeClass("active");
-		// Save the currently shown tab
-		lastShownCivCardId1 = tabId;
-
-		// Only show the current tab
-		$(tabId).addClass("active");
-	});
-}
-
-
-/**
- * Load each civ tabs
- */
-function loadCivTabs() {
-	for (let civ of civList) {
-		var civId = civ.toLowerCase() + "Tab";
-		var civHref = "#" + civ.toLowerCase() + "Content";
-
-		$("ul#civMilitaryTabs").append(
-			$('<li>').attr("class", "nav-item")
-					 .append(
-				$('<a>').attr("class", "nav-link")
-						.attr("id", civId)
-						.attr("data-toggle", "tab")
-						.attr("href", civHref)
-						.attr("role", "tab")
-						.append(civ)
-			)
-		);
-
-		// Add an active pane
-		if (civ === activeCiv) {
-			$("ul#civMilitaryTabs li a").addClass("active");
-		}
+	getCivList = function() {
+		return m_civList;
 	}
-}
+
+	getActiveCiv = function() {
+		return m_activeCiv;
+	}
 
 
-/**
- * Load each civilization's military composition
- */
-function loadCivMilitaryComposition() {
-	$.ajax({
-		type: "GET",
-		url: "civComposition.xml",
-		dataType: "xml",
-		success: function(xmlData) {
-			civs = $(xmlData).find("civ");
-			for (i = 0; i < civs.length; i++) {
-				var curCiv = civs[i];
-				var civName = $(curCiv).find("name").text();
-				var primaryUnits = $(curCiv).find("primary").text();
-				var supportingUnits = $(curCiv).find("support").text();
-				var notes = $(curCiv).find("notes").text();
-				
-				var civId = civName.toLowerCase() + "Content";
+	/**
+	 * Add functionality to the military cards
+	 */
+	addMilitaryCardUnitFunc = function() {
+		$("#civMilitaryTabs li a").on("click", function(e) {
+			e.preventDefault();
+			// Obtain the ID associated with the tab
+			var curCiv = "#" + $(this).text().toLowerCase();
 
-				$("div#civMilitaryContent").append(
-					$('<div>').attr("class", "tab-pane card col-sm-10 card-body normalFontWeight")
+			// Turn off previously active card's tab's content
+			$(m_activeCiv + "Content").removeClass("active");
+			// Remove the bottom border
+			$(m_activeCiv + "Tab").parent().removeClass("bottomBorder");
+
+			// Only show the current tab's content
+			$(curCiv + "Content").addClass("active");
+			// Add the bottom border to the tab
+			$(curCiv + "Tab").parent().addClass("bottomBorder");
+
+			// Save the currently shown tab
+			m_activeCiv = curCiv;
+		});
+	}
+
+
+	/**
+	 * Load each civ tabs
+	 * 
+	 * @param m_civList -> The alphabetically-sorted list of all civilizations
+	 */
+	loadCivTabs = function() {
+		for (let civ of m_civList) {
+			var civId = civ.toLowerCase() + "Tab";
+			var civHref = "#" + civ.toLowerCase() + "Content";
+
+			$("ul#civMilitaryTabs").append(
+				$('<li>').attr("class", "nav-item")
+				.append(
+					$('<a>').attr("class", "nav-link")
 					.attr("id", civId)
-					.attr("role", "tabpanel")
-					.append(
-						$('<p>').append("<strong>PRIMARY UNITS: </strong>")
-						.append(primaryUnits)
-					).append(
-						$('<p>').append("<strong>SUPPORTING UNITS: </strong>")
-						.append(supportingUnits)
-					).append(
-						$('<p>').append("<strong>NOTES: </strong>")
-						.append(notes)
-					)
-				);
+					.attr("data-toggle", "tab")
+					.attr("href", civHref)
+					.attr("role", "tab")
+					.append(civ)
+				)
+			);
 
-				// Add an active pane
-				if (civName === activeCiv) {
-					$("div#civMilitaryContent div").addClass("active");
-				}
+			// Add an active pane
+			if (civ === m_activeCiv) {
+				var curAnchorLink = "ul#civMilitaryTabs li a#" + civId;
+				$(curAnchorLink).addClass("active");
+				$(curAnchorLink).parent("li.nav-item").addClass("bottomBorder");
 			}
 		}
-	});
+	}
+
+
+	/**
+	 * Load each civilization's military composition
+	 */
+	loadCivMilitaryComposition = function() {
+		$.ajax({
+			type: "GET",
+			url: "civComposition.xml",
+			dataType: "xml",
+			success: function(xmlData) {
+				civs = $(xmlData).find("civ");
+				for (i = 0; i < civs.length; i++) {
+					var curCiv = civs[i];
+					var civName = $(curCiv).find("name").text();
+					var primaryUnits = $(curCiv).find("primary").text();
+					var supportingUnits = $(curCiv).find("support").text();
+					var notes = $(curCiv).find("notes").text();
+					
+					var civId = civName.toLowerCase() + "Content";
+
+					$("div#civMilitaryContent").append(
+						$('<div>').attr("class", "tab-pane card col-sm-10 card-body normalFontWeight")
+						.attr("id", civId)
+						.attr("role", "tabpanel")
+						.append(
+							$('<p>').append("<strong>PRIMARY UNITS: </strong>")
+							.append(primaryUnits)
+						).append(
+							$('<p>').append("<strong>SUPPORTING UNITS: </strong>")
+							.append(supportingUnits)
+						).append(
+							$('<p>').append("<strong>NOTES: </strong>")
+							.append(notes)
+						)
+					);
+
+					// Add an active pane
+					if (civName === m_activeCiv) {
+						$("div#" + civId).addClass("active");
+					}
+				}
+			}
+		});
+	}
+
+	return {
+		getCivList: getCivList,
+		getActiveCiv: getActiveCiv,
+		addMilitaryCardUnitFunc: addMilitaryCardUnitFunc,
+		loadCivTabs: loadCivTabs,
+		loadCivMilitaryComposition: loadCivMilitaryComposition
+	};
 }
