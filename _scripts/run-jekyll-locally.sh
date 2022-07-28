d1fbc395b2731d00fc3f5e1aea387c547d941e5f
Todd Nguyen <todd.nguyen47@gmail.com>
2022-07-28 09:52:18 -0700

    Trying Jekyll site

---
+++ _scripts/run-jekyll-locally.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+bundle exec jekyll serve
--- notes/aws-notes/convert-to-html.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-# asciidoctor is a Ruby gem
-asciidoctor aws-notes.adoc --out-file index.html
